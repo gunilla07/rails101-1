f2bd336964d67e724319e274edadb84913f0576d
gunilla <gunilla1148@gmail.com>
2017-03-31 14:10:53 +0800

    join & quit group action

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -53,6 +53,33 @@ redirect_to groups_path, alert:"Group deleted"
 
 end
 
+def join
+  @group = Group.find(params[:id])
+
+  if !current_user.is_member_of?(@group)
+    current_user.join!(@group)
+    flash[:notice] = "加入本讨论版成功！"
+  else
+    flash[:warning]= "你已经是本讨论版成员了！"
+  end
+
+  redirect_to group_path(@group)
+end
+
+def qiut
+  @group = Group.find(params[:id])
+
+  if current_user.is_member_of?(@group)
+    current_user.quit!(@group)
+    flash[:alert] = "已退出本讨论版！"
+  else
+    flash[:warning] = "你不是本讨论版成员，怎么推出 XD"
+  end
+
+  redirect_to group_path(@group)
+end
+
+
 
 private
 
